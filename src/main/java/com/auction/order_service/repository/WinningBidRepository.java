bf614aba608948a0ad355d06d2300e97edc0d9fb
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2024-12-18 08:17:24 +0000

    This will be removed as it belongs in the product service.

--- src/main/java/com/auction/order_service/repository/WinningBidRepository.java
+++
@@ -1,7 +0,0 @@
-package com.auction.order_service.repository;
-
-import com.auction.order_service.model.WinningBid;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WinningBidRepository extends JpaRepository<WinningBid, Long> {
-}
