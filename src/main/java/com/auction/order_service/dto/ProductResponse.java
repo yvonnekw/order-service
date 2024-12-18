2ae813916a85503cf95cd284684c1e4bd212b234
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2024-12-18 08:16:14 +0000

    updated to suite the requirement.

--- src/main/java/com/auction/order_service/dto/ProductResponse.java
+++ src/main/java/com/auction/order_service/dto/ProductResponse.java
@@ -1,8 +1,26 @@
 package com.auction.order_service.dto;
 
 
+
+
+import java.math.BigDecimal;
+
+
 public record ProductResponse(
-        Long productId
+        Long productId,
+        String username,
+        String productName,
+        String brandName,
+        String description,
+        BigDecimal startingPrice,
+        BigDecimal buyNowPrice,
+        String colour,
+        String productSize,
+        double quantity,
+        boolean isAvailableForBuyNow,
+        boolean isSold
+
 
         ) {
+
 }
